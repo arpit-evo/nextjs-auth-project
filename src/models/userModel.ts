18c612145a6b6d2885338235841935ac035b05c2
arpit-evo <arpit.m@evolutioncloud.in>
2024-06-27 13:53:48 +0530

    feat: add user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,51 @@
+import { Schema, Document, model, models } from "mongoose";
+
+export interface IUser extends Document {
+  username: String;
+  email: string;
+  password: string;
+  isVerified?: boolean;
+  isAdmin?: boolean;
+  forgotPasswordToken?: string;
+  forgotPasswordTokenExpiry?: Date;
+  verifyToken?: string;
+  verifyTokenExpiry?: Date;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: {
+      type: String,
+      required: [true, "username is required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.users || model<IUser>("users", userSchema);
+
+export default User;
