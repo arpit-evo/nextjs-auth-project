7a1f02dc023478e140bf5d802f714184cab0e82f
arpit-evo <arpit.m@evolutioncloud.in>
2024-06-27 13:51:11 +0530

    feat: add helper function of sending mail

---
+++ src/helpers/mailer.ts
@@ -0,0 +1,60 @@
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+import User from "@/models/userModel";
+
+export enum EmailType {
+  verify = "VERIFY",
+  forgot = "RESET",
+}
+
+export const sendMail = async ({
+  email,
+  emailType,
+  userId,
+}: {
+  email: string;
+  emailType: EmailType;
+  userId: string;
+}) => {
+  try {
+    const hashedToken = await bcryptjs.hash(userId, 10);
+
+    if (emailType === EmailType.verify) {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === EmailType.forgot) {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "8292fbec4b20ee", // this should be in env file
+        pass: "1858fc6a4143d3", // this should be in env file
+      },
+    });
+
+    const mailOptions = {
+      from: "maddison53@ethereal.email",
+      to: email,
+      subject:
+        emailType === EmailType.verify
+          ? "Verify your email"
+          : "Reset your password",
+      html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
+            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/verifyemail?token=${hashedToken}</p>`
+    };
+
+    const mailResponse = await transporter.sendMail(mailOptions);
+
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
